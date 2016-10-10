9af9dd0ca905135903e1e18ea56152c1fa4fef02
= <=>
2016-10-10 22:00:01 +0800

    arp xjtuwlan

--- router_fingerpringting/acitve/arp/arp_send.py
+++ router_fingerpringting/acitve/arp/arp_send.py
@@ -2,11 +2,11 @@ from scapy.all import *
 HOST_MAC = "34:23:87:45:78:21"
 # HOST_IP = "192.168.31.239"
 # DST_IP = "192.168.31.1"
-HOST_IP = "10.170.123.230"
-DST_IP = "10.170.127.254"
+HOST_IP = "10.164.170.111"
+DST_IP = "10.164.175.254"
 
 eth=Ether(src=HOST_MAC,type=0x0806)
 arp=ARP(hwtype=0x0001,ptype=0x0800,op=0x0001,hwsrc=HOST_MAC,psrc=HOST_IP,pdst=DST_IP)
 a = eth/arp
-for i in range(200):
+for i in range(1000):
 	recv = srp1(a)
